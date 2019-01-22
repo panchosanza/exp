05b4bcf7d715d18c16b98ea4bf4916c843b9aad7
Pancho Sanza <panchosanza@protonmail.com>
2019-01-22 10:42:33 -0600

    Image flipping problem.

--- py/flip_image.py
+++ py/flip_image.py
@@ -1,35 +1,42 @@
-def flip_invert_image(iarr):
-    """
-    Flip and invert the items in the matrix, as if it were an image.
+class Solution:
 
-    First, you must invert the array in place, then flip the bits and such.
-    normal:
-    1 1 0
-    1 0 1
-    0 0 0
-    
-    inverted:
-    0 1 1
-    1 0 1
-    0 0 0
+    def flipAndInvertImage(self, A):
+        """
+        Flip and invert the items in the matrix, as if it were an image.
 
-    return:
-    1 0 0
-    0 1 0
-    1 1 1
-    """
-    
-    for x in iarr:
-        for y range(len(y // 2)):
-            opposite_index = y - (y + 1)
-            current_opposite = x[opposite_index]
-            y = int(not(x))
-    return iarr
+        First, you must invert the array in place, then flip the bits and such.
+        normal:
+        1 1 0
+        1 0 1
+        0 0 0
+
+        inverted:
+        0 1 1
+        1 0 1
+        0 0 0
+
+        return:
+        1 0 0
+        0 1 0
+        1 1 1
+        """
+
+        for x in A:
+            for y in range(len(x) // 2):
+                index_offset = -(y + 1)
+                current = int(not(x[y]))
+                current_opposite = int(not(x[index_offset]))
+                x[y] = current_opposite
+                x[index_offset] = current
+            if not (len(x) % 2 == 0):
+                middle_index = (len(x) // 2)
+                x[middle_index] = int(not(x[middle_index]))
+        return A
 
 
 def test_flip_image():
-    assert flip_invert_image([[1,1,0],[1,0,1],[0,0,0]]) == [[1,0,0],[0,1,0],[1,1,1]]
+    assert Solution().flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]]) == [[1,0,0],[0,1,0],[1,1,1]]
 
 
 def test_flip_image2():
-    assert flip_invert_image([[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]) == [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+    assert Solution().flipAndInvertImage([[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]) == [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
